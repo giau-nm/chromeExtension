0faf47b21000e6599cb5d583a4f088ca06b69db5
giau-nm <giaunm.56@gmail.com>
2018-09-26 17:23:51 +0700

    init base

--- eventPage.js
+++ eventPage.js
@@ -1,23 +1,26 @@
 var userId = 0;
-chrome.storage.sync.get('search_user_id', function (storage) {
+chrome.storage.local.get('search_user_id', function (storage) {
     if (storage.search_user_id) {
         userId = parseInt(storage.search_user_id);
     } else {
         var d = new Date();
         userId = d.getTime();
-        chrome.storage.sync.set('search_user_id',userId);
+        chrome.storage.local.set('search_user_id',userId);
     }
 })
 var notificationsId = 'google-search-string-id';
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    $.post("http://google-search.esy.es/public/api/post/search",
+    $.post("http://google-search.esy.es/public/api/search",
     {
         user_id: userId,
         search_str: request.searchStr
     },
     function(data, status){
-        chrome.notifications.create(notificationsId, data);
+        if (data.isShowNotification === true) {
+            chrome.notifications.create('google-search-string-id', data.notificationData);
+        }
+        
     });
 
 })
